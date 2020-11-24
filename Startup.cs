a4ee511ae1d33268b95b6540c92a1537f66be8e1
endilar <endilar29@gmail.com>
2020-11-24 21:51:42 +0200

    upgrade

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace dotnet_rpg
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x =>
-                x.UseMySql(ServerVersion.AutoDetect(Configuration.GetConnectionString("DefaultConnection"))));
+                x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
